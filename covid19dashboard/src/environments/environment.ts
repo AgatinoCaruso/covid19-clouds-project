8a9e361e0aed0e09501886d0b85df1d7133031fd
AgatinoCaruso <agatinocaruso96@live.it>
2020-11-13 00:42:06 +0100

    routing added

--- covid19dashboard/src/environments/environment.ts
+++ covid19dashboard/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAaC7ORMDZgA14jrntM5T23q393mhVTQNo",
+    authDomain: "covid19-dashboard-295118.firebaseapp.com",
+    databaseURL: "https://covid19-dashboard-295118.firebaseio.com",
+    projectId: "covid19-dashboard-295118",
+    storageBucket: "covid19-dashboard-295118.appspot.com",
+    messagingSenderId: "1029365794027",
+    appId: "1:1029365794027:web:9e41aad9099b6e39d3aaae",
+    measurementId: "G-430XTVM1YY"
+  }
 };
 
 /*
